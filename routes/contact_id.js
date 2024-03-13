355ee662116dfb8be98b5822a26e402f12a7cbe9
Edward Del Rosario <edwarddr23@gmail.com>
2024-03-13 18:57:19 -0400

    Cleaned up console.log commands in code

--- routes/contact_id.js
+++ routes/contact_id.js
@@ -11,10 +11,7 @@ const logged_in = (req, res, next) => {
 }
 
 router.get('/:contact_id', async(req, res) => {
-    console.log('contact_id.js: GET(): req.params', req.params);
     const contact = await req.db.findContactById(req.params.contact_id);
-    // console.log('contact_id.js: GET(): contact:', contact);
-    // req.session.id
     res.render('contact_info', {contact: contact});
 })
 
@@ -24,23 +21,18 @@ router.get('/:contact_id/edit', logged_in, async(req, res) => {
 })
 
 router.post('/:contact_id/edit', logged_in, async(req, res) => {
-    console.log('contact_id.js: POST(): req.params:', req.params);
     const contact = await req.db.findContactById(req.params.contact_id);
-    // console.log('contact_id.js: POST(): contact:', contact);
     await req.db.recordContact(req.body, req.params.contact_id, contact.user_id)
     res.redirect('/' + contact.id);
 })
 
 router.get('/:contact_id/delete', logged_in, async(req, res) => {
     const contact = await req.db.findContactById(req.params.contact_id);
-    console.log('contact_id.js: delete GET(): req.params:', req.params);
     res.render('delete', {contact: contact});
 })
 
 router.post('/:contact_id/delete', logged_in, async(req, res) => {
-    // console.log('contact_id.js: delete POST(): req.params:', req.params);
     const contact = await req.db.findContactById(req.params.contact_id);
-    // console.log('contact_id.js: delete POST(): contact:', contact);
     await req.db.deleteContactById(contact.id);
     res.redirect('/');
 })
--- routes/contact_list.js
+++ routes/contact_list.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.get('/', async(req, res) => {
     const contacts = await req.db.findContacts();
-    console.log('contact_list.js: contacts:', contacts);
     res.render('contact_list', {contacts: contacts});
 });
 
